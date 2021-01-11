fe70481757ca1034e5acee76a3a7900a8ac42941
Jonathan Robinson <jdswebservice@gmail.com>
2021-01-10 19:31:44 -0500

    Fixed Output Log File Tee

--- includes/cleanup.sh
+++ includes/cleanup.sh
@@ -1,8 +1,8 @@
 {
 	printMsg "cleanup.sh" "Clean Up Some Things Before The Installation Processes"
 
-	
+
 	sudo apt-get remove docker docker-engine docker.io containerd runc
 	
 	sudo apt update
-} 2>&1 | tee /tmp/ls.txt
+} 2>&1 | tee $SCRIPTLOG
